3710e3b0c4c70ffc6f6315755b20a5005ef91af9
Mario Gender <Gender.Mario@outlook.com>
2020-02-23 15:35:19 +0100

    End of section 5

--- DatingApp.API/DTOs/UserForRegisterDto.cs
+++ DatingApp.API/DTOs/UserForRegisterDto.cs
@@ -11,6 +11,7 @@ namespace DatingApp.API.DTOs
         [Required]
         public string UserName { get; set; }
 
+        
         [Required]
         [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 character long")]
         public string  Password { get; set; }
